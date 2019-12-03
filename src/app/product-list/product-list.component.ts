45a06138f3151507d45c613e6faac9eaf773f3ea
Clare Blackburne <chblackburne@gmail.com>
2019-12-03 10:23:30 +0000

    wip responsive grid

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,18 @@ console.log(products)
 })
 export class ProductListComponent {
   products = products;
+  breakpoint;
   share() {
     window.alert("The product has been shared!");
   }
   onNotify() {
     window.alert("You will be notified when the product goes on sale");
+  }
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 400) ? 1 : 6;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
   }
 }
