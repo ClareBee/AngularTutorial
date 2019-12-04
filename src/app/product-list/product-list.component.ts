061f44e005176618cf329ca9592ddd0d0de92d5f
Clare Blackburne <chblackburne@gmail.com>
2019-12-04 17:00:30 +0000

    basic responsiveness

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,17 +11,20 @@ console.log(products)
 export class ProductListComponent {
   products = products;
   breakpoint;
+
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 940) ? 1 : 4;
+  }
+
   share() {
     window.alert("The product has been shared!");
   }
+
   onNotify() {
     window.alert("You will be notified when the product goes on sale");
   }
-  ngOnInit() {
-    this.breakpoint = (window.innerWidth <= 400) ? 1 : 6;
-  }
 
   onResize(event) {
-    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
+    this.breakpoint = (event.target.innerWidth <= 940) ? 1 : 4;
   }
 }
